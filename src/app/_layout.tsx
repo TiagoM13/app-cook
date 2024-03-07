624f996058b9d3b27208ffe80c67e4c150774b9d
TiagoM13 <thyagomotha2000@gmail.com>
2024-03-06 21:35:45 -0300

    feat: initial app

---
+++ src/app/_layout.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { Slot } from 'expo-router';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins'
+
+const Layout: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  })
+
+  if (!fontsLoaded) {
+    return
+  }
+
+  return fontsLoaded ? <Slot /> : null;
+}
+
+export default Layout;
